1152abac8665ca23999698ba94d1d35efb3431d6
Lkarimic <56609213+Lkarimic@users.noreply.github.com>
2020-04-14 19:19:58 -0400

    added sound effects

---
+++ Prison Escape/Assets/Scripts/GameAssets.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// House assets
+// Obstacles, etc.
+
+public class GameAssets : MonoBehaviour {
+	
+	private int score = 0;
+    public Text txt;
+    public Text health;
+	private bool recentImpact = false;
+
+    private int Alive=3;
+
+    public static GameAssets instance;
+	
+	public static GameAssets GetInstance() {
+		return instance;
+	}
+
+	public void Awake() {
+		instance = this;
+	}
+
+    public void Start() {
+        health.text = "Health: " + Alive;
+    }
+	
+	// Stores assets for obstacles
+	public Transform jumpObsBody;
+	public Transform duckObsBody;
+	public Transform diveObsBody;
+    public Transform bgBody;
+    public Transform mgBody;
+    public Transform fgBody;
+    public Transform newGroundBody;
+
+    public void increaseScore() {
+		if (recentImpact == false) {
+			score++;
+			Debug.Log("Current Score: " + score);
+			txt.text = "Current Score: " + score;
+		}
+    }
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void resetScore() {
+		score = 0;
+        
+		Debug.Log("Current Score: " + score);
+        txt.text = "Current Score: " + score;
+       
+    }
+    public int reducehealth()
+    {
+        Alive -= 1;
+        health.text = "Health: " + Alive;
+        return Alive;
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Prison Escape
+
+Play: https://www.wurmatron.io/Prison-Escape/
+
+# Controls
+
+Jump: `W` or `Up Arrow`
+
+Dive: `D` or `Right Arrow`
+
+Duck: `S` or `Down Arrow`
